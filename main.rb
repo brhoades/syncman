7fcaafcf89662d2378f1ac970dbb54c35be09182
Billy Rhoades <billyarhoades@gmail.com>
2013-12-18 19:04:34 -0600

    Now compiles the file data into a useful format.

--- main.rb
+++ main.rb
@@ -4,8 +4,9 @@ require 'net/ssh'
 cfg = YAML.load File.open "config.yml"
 sshc = cfg['ssh']
 downc = cfg['download']
-print cfg, "\n\n"
 files = ""
+folders = {} #Our folder indicies that point to our files
+delete = [] #Put things to delete in here as we go
 
 #Contact the server via SSH and get a file readout with ls
 Net::SSH.start( sshc['server'], sshc['username'], :password => [sshc['pass']], :port => sshc['port'] ) do |ssh|
@@ -14,10 +15,36 @@ files = ssh.exec! "cd " + sshc['directory'] + "&& find -type f -newermt @#{Date.
 end
 
 #Take each line of the string recieved above and split it into an array of its values
-files = files.lines.map( &:chomp ).map { |l| l.split "\t" }
+files = files.lines.map { |l| l.chomp.split "\t" }
 
 files.each do |filex|
+  filex[0].gsub!( /^\.\//, '' )
+  folder = []
+  fn = filex[0]
+  size = filex[1]
+  folder = []
   
+  #Remove the folder names (if there are any) and make them later
+  if fn.match '/'
+    fnarray = fn.split '/'
+    while fnarray.size > 1
+      folder.push fnarray.shift
+    end
+    
+    fn = fnarray.join
+  end
   
-  
+  folder = folder.join '/'
+  data = [fn, size]
+  if folder.size == 0
+    folder = ""
+  end
+
+  if folders.has_key? folder
+    folders[folder].push data
+  else
+    folders[folder] = [data]
+  end  
 end  
+
+print folders
